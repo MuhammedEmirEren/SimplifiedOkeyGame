d78d38ff1e874662b43b7748baa5bc5f5df26dbe
Alp Eren Köken <eren.koken@ug.bilkent.edu.tr>
2024-02-17 15:00:51 +0300

    Addition to a method
    added a forgotten opportunity to a method

--- SimplifiedOkeyGame.java
+++ SimplifiedOkeyGame.java
@@ -79,6 +79,7 @@ public class SimplifiedOkeyGame {
      */
     public String getLastDiscardedTile() {
         if(lastDiscardedTile!=null){
+            players[currentPlayerIndex].addTile(lastDiscardedTile);
             return lastDiscardedTile.toString();
         }
         return "There is no tile in the table yet.";
@@ -131,7 +132,7 @@ public class SimplifiedOkeyGame {
      * if multiple players have the same length may return multiple players
      */
     public Player[] getPlayerWithHighestLongestChain() {
-        
+
         ArrayList<Player> winners=new ArrayList<>();
         int maxChain=0;
         for(Player p:players){
